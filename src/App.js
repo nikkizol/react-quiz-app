731f8b7899838c351e796c997e4e8819417c32e4
MZ <nikita.zholkovskyi@gmail.com>
2020-12-18 15:44:09 +0100

    looping over the questions and displaying answers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import React, { useState } from "react";
 
 function App() {
+  const questions = [
+    {
+      questionText: "What is the capital of Italy?",
+      answerOptions: [
+        { answerText: "Milan", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Rome", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Amazon?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: true },
+        { answerText: "Elon Musk", isCorrect: false },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the capital of Australia?",
+      answerOptions: [
+        { answerText: "Canberra", isCorrect: true },
+        { answerText: "Melbourbe", isCorrect: false },
+        { answerText: "Sydney", isCorrect: false },
+        { answerText: "Viktoria", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "React was created by:",
+      answerOptions: [
+        { answerText: "Samsung", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Goolge", isCorrect: false },
+        { answerText: "Faceboook", isCorrect: true },
+      ],
+    },
+  ];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleAnswerButtonClick = (answerOption) => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      alert("you reached the end of the quiz");
+    }
+  };
+
   return (
     <div className="app">
       {false ? (
@@ -11,17 +60,18 @@ function App() {
         <>
           <div className="question-section">
             <div className="question-count">
-              <span>Question 1</span>/{questions.length}
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
             </div>
             <div className="question-text">
-              This is where the question text should go
+              {questions[currentQuestion].questionText}
             </div>
           </div>
           <div className="answer-section">
-            <button>Answer 1</button>
-            <button>Answer 2</button>
-            <button>Answer 3</button>
-            <button>Answer 4</button>
+            {questions[currentQuestion].answerOptions.map((answer) => (
+              <button onClick={() => handleAnswerButtonClick()}>
+                {answer.answerText}
+              </button>
+            ))}
           </div>
         </>
       )}
