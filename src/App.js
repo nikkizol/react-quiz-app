90fea64d29281c6685dfee74b6715f7bf06281e6
MZ <nikita.zholkovskyi@gmail.com>
2020-12-18 15:30:13 +0100

    returned html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {false ? (
+        <div className="score-section">
+          You scored 1 out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question 1</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              This is where the question text should go
+            </div>
+          </div>
+          <div className="answer-section">
+            <button>Answer 1</button>
+            <button>Answer 2</button>
+            <button>Answer 3</button>
+            <button>Answer 4</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
